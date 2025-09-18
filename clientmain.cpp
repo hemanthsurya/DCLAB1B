b60adef868fca0646c7db3476809467fd1ec3e6f
hemanth <khemanthsura@gmail.com>
2025-09-18 22:20:49 +0200

    Message transport implementation
    The message sending and receiving with retry function implemented.

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-/* You will to add includes here */
-
-
-// Included to get the support library
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include <calcLib.h>
-
-
 #include "protocol.h"
+
+static ssize_t send_with_retry(int sock,
+                               const void *buf, size_t len,
+                               void *rbuf, size_t rlen,
+                               struct sockaddr *srv, socklen_t slen,
+                               struct sockaddr *from, socklen_t *flen)
+{
+  const int max_attempts = 3;
+  const int timeout  = 2;
+  
+  for (int attempt = 1; attempt <= max_attempts; ++attempt) {
+      if (sendto(sock, buf, len, 0, srv, slen) != (ssize_t)len) {
+          printf("ERROR:SENDING");
+          return -1;
+      }
+
+      fd_set fds;
+      FD_ZERO(&fds);
+      FD_SET(sock, &fds);
+      struct timeval tv;
+      tv.tv_sec  = timeout;
+      tv.tv_usec = 0;
+
+      int rv = select(sock + 1, &fds, NULL, NULL, &tv);
+      if (rv > 0) {
+          ssize_t got = recvfrom(sock, rbuf, rlen, 0, from, flen);
+          if (got < 0) {
+              printf("ERROR:RECEIVING");
+              return -1;
+          }
+          return got;
+      }
+      if (rv < 0) {
+          printf("ERROR:FILE DESCRIPTOR");
+          return -1;
+      }
+  }
+  return -2;
+}
+
 
 int main(int argc, char *argv[]){
   
@@ -18,6 +57,11 @@ int main(int argc, char *argv[]){
   const char *desthost = argv[1];
   int destport = atoi(argv[2]);
   printf("Connecting to %s:%d\n", desthost, destport);
-  
+
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sock < 0) {
+      perror("socket");
+      return 1;
+  }
 
 }
